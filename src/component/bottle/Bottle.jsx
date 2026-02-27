bd68e508de8a7c7c04fecd875c82d626777b8b3a
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-02-27 10:26:48 +0600

    all bottle show

--- src/component/bottle/Bottle.jsx
+++ src/component/bottle/Bottle.jsx
@@ -1,11 +1,15 @@
-
-const Bottle = ({bottle}) => {
+import "./bottle.css"
+const Bottle = ({bottle, handleAddToCart}) => {
     // console.log(bottle);
-    const {img} = bottle
+    const {img, price, stock, name } = bottle
     
     return (
-        <div>
+        <div className="card">
             <img src={img} alt="" />
+            <h3>{name}</h3>
+            <p>Price: ${price}</p>
+            <p>Stock: {stock} Unit</p>
+            <button onClick={() => handleAddToCart(bottle)}>Buy Now</button>
         </div>
     );
 };
--- src/component/bottles/Bottles.jsx
+++ src/component/bottles/Bottles.jsx
@@ -1,16 +1,31 @@
-import { use } from "react";
+import { use, useState } from "react";
 import Bottle from "../bottle/Bottle";
+import "./bottles.css"
 
-const Bottles = ({bottlesPromise}) => {
+const Bottles = ({ bottlesPromise }) => {
+
+    const [cart, setCart] = useState([])
+
+    const handleAddToCart = (bottle) => {
+        console.log("click cart");
+        console.log(bottle);
+        
+        
+    }
     const loadedBottles = use(bottlesPromise)
-    console.log(loadedBottles);
-    
+    // console.log(loadedBottles);
+
     return (
         <div>
             <h2>Awesome Bottle {loadedBottles.length}</h2>
-            {
-                loadedBottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
-            }
+            <div className="bottles-Container">
+                {
+                    loadedBottles.map(bottle => <Bottle 
+                        key={bottle.id} 
+                        handleAddToCart={handleAddToCart}
+                        bottle={bottle}></Bottle>)
+                }
+            </div>
         </div>
     );
 };
